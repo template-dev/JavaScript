a1b24d04f4b434e74377c1bc23f61b670dba064d
Vladislav <jukov801@gmail.com>
2026-03-11 19:10:02 +0300

    Added DOMContentLoaded

--- tabs/app.js
+++ tabs/app.js
@@ -1,25 +1,27 @@
-const tabs = document.querySelector('.tabs');
-const tabButtons = tabs.querySelectorAll('.tab-btn');
-const tabContents = tabs.querySelectorAll('.tab-panel');
+document.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelector('.tabs');
+    const tabButtons = tabs.querySelectorAll('.tab-btn');
+    const tabContents = tabs.querySelectorAll('.tab-panel');
 
-let activeButton = tabs.querySelector('.tab-btn.active');
-let activePanel = tabs.querySelector('.tab-panel.active');
+    let activeButton = tabs.querySelector('.tab-btn.active');
+    let activePanel = tabs.querySelector('.tab-panel.active');
 
-tabButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        if (button === activeButton)
-            return;
+    tabButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            if (button === activeButton)
+                return;
 
-        activeButton.classList.remove('active');
-        activeButton.setAttribute('aria-selected', 'false');
+            activeButton.classList.remove('active');
+            activeButton.setAttribute('aria-selected', 'false');
 
-        activePanel.classList.remove('active');
+            activePanel.classList.remove('active');
 
-        button.classList.add('active');
-        button.setAttribute('aria-selected', 'true');
-        tabContents[index].classList.add('active');
+            button.classList.add('active');
+            button.setAttribute('aria-selected', 'true');
+            tabContents[index].classList.add('active');
 
-        activeButton = button;
-        activePanel = tabContents[index];
+            activeButton = button;
+            activePanel = tabContents[index];
+        });
     });
 });
